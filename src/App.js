ba798b764b4a2ebbc7c3833a5fc0b5a4799854f3
janahasan <99503558+janahasa@users.noreply.github.com>
2023-11-20 17:33:11 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography} from '@material-ui/core'; // Use this import for Material-UI v5
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <Typography variant='h1'> hello</Typography>
+    </div>
+  );
+}
+
+export default App;
